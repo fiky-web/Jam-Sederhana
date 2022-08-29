1e8e90fa0650cc680f9349304f0da24f3594f26b
Fiky Anggara <71414823+fiky-web@users.noreply.github.com>
2022-08-29 18:41:16 +0700

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,7 @@
 import type { Component, Accessor } from "solid-js";
 import "@fontsource/cousine/400.css";
 import styles from "../styles/Card.module.css";
+
 export interface CardProps {
   hour: Accessor<string>;
   minute: Accessor<string>;
