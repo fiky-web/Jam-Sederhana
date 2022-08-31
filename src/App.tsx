267d4cbe86dbb3f28c2fc6129b3a0f27c423b8e8
Fiky Anggara <71414823+fiky-web@users.noreply.github.com>
2022-08-31 18:12:21 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
 import { onMount, createSignal, createMemo } from "solid-js";
 import type { Component } from "solid-js";
 import Card from "./components/Card";
+import styles from "./styles/App.module.css";
