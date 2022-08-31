7f60c8c08df389505e42824a7cfbcba31717c1b1
Fiky Anggara <71414823+fiky-web@users.noreply.github.com>
2022-08-31 18:13:12 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,3 +6,5 @@ import styles from "./styles/App.module.css";
 
 const updateString = (time: number): string =>
   time < 10 ? `0${(time as unknown) as string}` : ((time as unknown) as string);
+  const App: Component = () => {
+  const [time, setTime] = createSignal<Date>(new Date());
