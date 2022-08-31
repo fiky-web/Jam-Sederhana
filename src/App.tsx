b888399936e2d6d95860038dd3762afa2e9fe93f
Fiky Anggara <71414823+fiky-web@users.noreply.github.com>
2022-08-31 18:12:58 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,5 +3,6 @@ import type { Component } from "solid-js";
 import Card from "./components/Card";
 
 import styles from "./styles/App.module.css";
+
 const updateString = (time: number): string =>
   time < 10 ? `0${(time as unknown) as string}` : ((time as unknown) as string);
